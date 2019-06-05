9442647047d7ac228bccd178fc0cabd68691b628
AgustinMoralejo <amoralejo@fi.uba.ar>
2019-06-05 15:00:12 -0300

    clases pico y hacha con sus subclases

--- test/fiuba/algo3/tp2/HachaMetalTest.java
+++ test/fiuba/algo3/tp2/HachaMetalTest.java
@@ -2,7 +2,8 @@ package fiuba.algo3.tp2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HachaMetalTest {
 
--- test/fiuba/algo3/tp2/PicoMaderaTest.java
+++ test/fiuba/algo3/tp2/PicoMaderaTest.java
@@ -2,7 +2,9 @@ package fiuba.algo3.tp2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class PicoMaderaTest {
 
