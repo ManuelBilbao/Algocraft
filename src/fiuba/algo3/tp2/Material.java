14eaf839547adb76fc7b83ee6b8955ba32e72f31
jocairo <mcairo@fi.uba.ar>
2019-06-05 23:13:57 -0300

    Pruebasmateriales

--- src/fiuba/algo3/tp2/Material.java
+++ src/fiuba/algo3/tp2/Material.java
@@ -1,13 +1,14 @@
 package fiuba.algo3.tp2;
 
-public class Material {
+public abstract class Material {
     protected int durabilidad;
 
-    public int getDurabilidad() {
-        return durabilidad;
-    }
+    public int getDurabilidad() { return durabilidad; }
+
+    public abstract void recibirGolpe(Herramienta herramienta);
 
     public void desgastar(int fuerza) {
+        if (durabilidad <= 0) throw new NoPoseeDurabilidadException();
         durabilidad = durabilidad - fuerza;
     }
 }
