adf15cdaf81a638f394a5a10c586339b9a664f4a
AgustinMoralejo <amoralejo@fi.uba.ar>
2019-06-12 20:26:18 -0300

    Merge branch 'master' of /home/usuario/IdeaProjects/Algocraft with conflicts.

--- src/fiuba/algo3/tp2/modelo/Mapa/Mapa.java
+++ src/fiuba/algo3/tp2/modelo/Mapa/Mapa.java
@@ -35,19 +35,81 @@ public class Mapa {
         return casilleros[fila][columna];
     }
 
+    public Casillero getCasillero(Posicion posicion) {
+        return casilleros[posicion.getFila()][posicion.getColumna()];
+    }
+
     public Posicion getPosicionJugador() {
         return posicionJugador;
     }
 
-    public void setPosicionJugador(Posicion posicionNueva) {
-        this.posicionJugador = posicionNueva;
+    public void inicializacionMateriales() {
+        inicializarDiamante();
+        inicializarMadera();
+        inicializarMetal();
+        inicializarPiedra();
     }
 
-    public void liberarCasillero(Posicion posicion) {
-        casilleros[posicion.getFila()][posicion.getColumna()].liberar();
+
+    public void inicializarMadera(){
+        int fila = 0;
+        int columna = 0;
+        int tope = 0;
+        while (tope < 200) {
+            fila = (int) (Math.random() * 30) + 1;
+            columna = (int) (Math.random() * 30) + 1;
+            if (casilleros[fila][columna].getContenido() == null) {
+                casilleros[fila][columna].ocupar("madera");
+                tope++;
+            }
+
+        }
     }
 
-    public void ocuparCasillero(Posicion posicion, Jugador jugador) {
-        casilleros[posicion.getFila()][posicion.getColumna()].ocupar(jugador);
+    public void inicializarPiedra(){
+        int fila = 0;
+        int columna = 0;
+        int tope = 0;
+        while (tope < 150) {
+            fila = (int) (Math.random() * 30) + 1;
+            columna = (int) (Math.random() * 30) + 1;
+            if (casilleros[fila][columna].getContenido() == null) {
+                casilleros[fila][columna].ocupar("piedra");
+                tope++;
+            }
+
+        }
     }
+
+
+    public void inicializarMetal(){
+        int fila = 0;
+        int columna = 0;
+        int tope = 0;
+        while (tope < 100) {
+            fila = (int) (Math.random() * 30) + 1;
+            columna = (int) (Math.random() * 30) + 1;
+            if (casilleros[fila][columna].getContenido() == null) {
+                casilleros[fila][columna].ocupar("metal");
+                tope++;
+            }
+
+        }
+    }
+
+    public void inicializarDiamante(){
+        int fila = 0;
+        int columna = 0;
+        int tope = 0;
+        while (tope < 50) {
+            fila = (int) (Math.random() * 30) + 1;
+            columna = (int) (Math.random() * 30) + 1;
+            if (casilleros[fila][columna].getContenido() == null) {
+                casilleros[fila][columna].ocupar("diamante");
+                tope++;
+            }
+
+        }
+    }
+
 }
